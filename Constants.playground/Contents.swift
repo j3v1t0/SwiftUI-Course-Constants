602af04332b2400474629e7ae840c84781a9239a
Alfonso Almonte <alfonsoalmonte@gmail.com>
2023-09-22 12:15:37 -0400

    Constants

---
+++ Constants.playground/Contents.swift
@@ -0,0 +1,9 @@
+import PlaygroundSupport
+//let is used for unique value
+let name = "Alfonso"
+print(name)
+/* Cannot assign to value: 'name' is a 'let' constant,
+ if you want to change the value, change 'let'to 'var'
+ to make it mutable
+ */
+name = "Alexander" //This line will not work
